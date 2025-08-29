dbc589b3332f3aea694f0fae03cbcc4d0ad11c2f
drjhero <james.thomas.herold@gmail.com>
2025-08-28 21:25:15 -0700

    Applies a handful of readability improvements

--- src/main/java/SAXParserExample.java
+++ src/main/java/SAXParserExample.java
@@ -15,16 +15,12 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXParserExample extends DefaultHandler {
 
-    List<Employee> myEmpls;
+    List<Employee> employeeList;
 
     private String tempVal;
 
     //to maintain context
     private Employee tempEmp;
-
-    public SAXParserExample() {
-        myEmpls = new ArrayList<Employee>();
-    }
 
     public void runExample() {
         parseDocument();
@@ -32,15 +28,13 @@ public class SAXParserExample extends DefaultHandler {
     }
 
     private void parseDocument() {
-
-        //get a factory
-        SAXParserFactory spf = SAXParserFactory.newInstance();
+        // Create a factory that creates new SAX parers
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
         try {
+            // Build a new parser from the factory
+            SAXParser sp = saxParserFactory.newSAXParser();
 
-            //get a new instance of parser
-            SAXParser sp = spf.newSAXParser();
-
-            //parse the file and also register this class for call backs
+            // Parses the file using the overriden methods below
             sp.parse("employees.xml", this);
 
         } catch (SAXException se) {
@@ -58,35 +52,44 @@ public class SAXParserExample extends DefaultHandler {
      */
     private void printData() {
 
-        System.out.println("No of Employees '" + myEmpls.size() + "'.");
+        System.out.println("No of Employees '" + employeeList.size() + "'.");
 
-        Iterator<Employee> it = myEmpls.iterator();
+        Iterator<Employee> it = employeeList.iterator();
         while (it.hasNext()) {
             System.out.println(it.next().toString());
         }
     }
 
-    //Event Handlers
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        //reset
+    // Called once at the beginning of parsing a new document
+    @Override
+    public void startDocument() {
+        employeeList = new ArrayList<>();
+    }
+
+    // Overrides the method that is called whenever the parser encounters the starting tag of an XML element
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        // Reset the temporary value we are storing
         tempVal = "";
         if (qName.equalsIgnoreCase("Employee")) {
-            //create a new instance of employee
+            // We encountered a new Employee tag and create a new instance of employee
             tempEmp = new Employee();
             tempEmp.setType(attributes.getValue("type"));
         }
     }
 
-    public void characters(char[] ch, int start, int length) throws SAXException {
+    // Overrides the method that is called whenever the parser encounters the characters in between XML start and
+    // end elements
+    @Override
+    public void characters(char[] ch, int start, int length) {
         tempVal = new String(ch, start, length);
     }
 
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-
+    // Overrides the method that is called whenever the parser encounters the ending tag of an XML element
+    @Override
+    public void endElement(String uri, String localName, String qName) {
         if (qName.equalsIgnoreCase("Employee")) {
-            //add it to the list
-            myEmpls.add(tempEmp);
-
+            employeeList.add(tempEmp);
         } else if (qName.equalsIgnoreCase("Name")) {
             tempEmp.setName(tempVal);
         } else if (qName.equalsIgnoreCase("Id")) {
@@ -94,12 +97,10 @@ public class SAXParserExample extends DefaultHandler {
         } else if (qName.equalsIgnoreCase("Age")) {
             tempEmp.setAge(Integer.parseInt(tempVal));
         }
-
     }
 
     public static void main(String[] args) {
-        SAXParserExample spe = new SAXParserExample();
-        spe.runExample();
+        SAXParserExample saxParserExample = new SAXParserExample();
+        saxParserExample.runExample();
     }
-
 }
